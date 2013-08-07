65ef365f8c090254345ec05cd01a63b2e5da0ba2
ivancduran <ivan.cduran@gmail.com>
2013-08-06 21:11:05 -0500

    v 0.6

--- js/kraken.js
+++ js/kraken.js
@@ -1,21 +1,8 @@
-(function(window,document,undefined){
+// Kraken Framework 0.6
+var Kraken = function(_options){
 $(document).ready(function(){
-console.time("t1");
-// Kraken Framework 0.5
-var _options = {
-	// pagesize : { state : false, width: 1024, },
-	// overflow : { state : false, x : false, y : true },
-	// bgimg : { state : false, img : '004', size : 'default'},
-	// bgcolor : { state : false, color : '0fa1e0' },
-	// largewrap : { state : false, rest : 'default' },
-	// wrapshadow : { state : false, intense : 'default', size : 'default'},
-	// slider : {state : false, auto : true, pager : false, nav : true, speed : 500},
-	// submenu : true,
-	// span : { state : false, multiply : '10' },
-	// interval : { state : false, timer : '2000'},
-	// hide : { state : false, resize : true, size : 700 },
-	// fonts : {},
-}
+
+console.time("Kraken Load Time");
 
 var _default = {
 	pagesize : { state : true, width: 1024, },
@@ -24,8 +11,8 @@ var _default = {
 	bgcolor : { state : true, color : '0fa1e0' },
 	largewrap : { state : true, rest : 'default' },
 	wrapshadow : { state : true, intense : 'default', size : 'default'},
-	slider : {state : true, auto : true, pager : false, nav : true, speed : 500},
-	submenu : true,
+	slider : { state : true, auto : true, pager : false, nav : true, speed : 500},
+	menu : { state : true, submenu: true, },
 	span : { state : true, multiply : '10' },
 	interval : { state : false, timer : '2000'},
 	hide : { state : true, resize : true, size : 700 },
@@ -48,7 +35,9 @@ if(kraken.bgcolor.state) $('html').css('background-color','#'+kraken.bgcolor.col
 // Wrap height of the screen
 if(kraken.largewrap.state){
 	var RestWarp = ( kraken.largewrap.rest == 'default' ) ? '20' : kraken.largewrap.rest;
-	$('.wrap').css('min-height',$(document).height() - RestWarp);
+	setTimeout(function(){
+		$('.wrap').css('min-height',$(document).height() - RestWarp);
+	},350);
 }
 // BorderShadow on Wrap shadow="on"
 if(kraken.wrapshadow.state) $('.wrap').addClass('shadowWrap');
@@ -69,20 +58,23 @@ $("#slider").responsiveSlides({
 });
 }
 //SubMenu
-if(kraken.submenu){
+
+if(kraken.menu.state){
+	$('.grids > #menu').css({'position':'relative', 'width':'auto', 'z-index':'8'});
 	$('.subMenu').hide();
 	var togglesub = true;
 	$('#soluciones').on('click',function(event){
 		event.preventDefault();
 		
 		if(togglesub){
-			$('.subMenu').show().effect("slide", { direction:'up', times:5 }, 450);
-			$('.subMenu').show().effect("highlight", { color: "#fff", times:5 }, 450);
+			$('.subMenu').show().effect("slide", { direction:'up', times: 5 }, 450);
+			$('.subMenu').show().effect("highlight", { color: "#fff", times: 5 }, 450);
 			togglesub = false;
 		}else{
 			$('.subMenu').hide('slow');
 			togglesub = true;
 		}
+
 	});
 
 		if($(document).width() > kraken.hide.size){
@@ -132,7 +124,7 @@ if(kraken.hide.state){
 					$(this).hide();
 				});
 
-				MenuBack();
+				// MenuBack();
 			}else{
 				$('.hide').each(function(){
 					$(this).show();
@@ -145,15 +137,17 @@ if(kraken.hide.state){
 function MenuBack(){
 		$(".nav a").on('click',function(event) {
 			if(!$('.menuBack').is(":visible")){
+			var altura = $('body').height();
 			   	event.preventDefault();
-				$('.nav').addClass('click');
-				$('.menuBack').animate({ opacity: .8 }).fadeIn();
+				// $('.nav ul li').addClass('click');
+				$('.nav ul li').css({'display' : 'block', 'margin' : '0 0 5px', 'z-index' : '999'});
+				$('.menuBack').css({'height' : altura }).animate({ opacity: .8 }).fadeIn();
 			}
 		});
 
 		$('.menuBack').on('click',function(){
 			$('.menuBack').animate({ opacity: 0}).fadeOut();
-			$('.nav').removeClass('click');
+			$('.nav ul li').removeAttr('style');
 		});
 }
 
@@ -169,7 +163,7 @@ $('.cta').on('click',function(){
 	
 });
 
-console.timeEnd("t1");
-});
+console.timeEnd("Kraken Load Time");
 
-})(this,this.document);
+});
+}
